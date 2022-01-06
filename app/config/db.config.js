c6ce26df020f043d695353ba11e008f1a2e2b5ae
jeffreyjgong <jeffrey.gong7@gmail.com>
2022-01-06 05:18:16 -0500

    Initial commit

---
+++ app/config/db.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+   HOST: "127.0.0.1",
+   USER: "root",
+   PASSWORD: "123456",
+   DB: "testdb",
+   dialect: "mysql",
+   socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock',
+   pool: {
+     max: 5,
+     min: 0,
+     acquire: 30000,
+     idle: 10000
+   }
+ };
