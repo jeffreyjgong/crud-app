143322700a337f46797e01edb2a9373bc30ae6b7
jeffreyjgong <jeffrey.gong7@gmail.com>
2022-01-06 20:57:14 -0500

    Minor fix in routes

--- app/routes/tutorial.routes.js
+++ app/routes/tutorial.routes.js
@@ -15,6 +15,9 @@ module.exports = app => {
    // Retrieve a single Tutorial with id
    router.get("/:id", tutorials.findOne);
 
+   // Update a Tutorial with id
+   router.put("/:id", tutorials.update);
+
    // Delete a Tutorial with id
    router.delete("/:id", tutorials.delete);
 
